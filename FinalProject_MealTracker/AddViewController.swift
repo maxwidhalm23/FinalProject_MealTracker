7aca39bd0cb44bdfc949489591494439ee4937f8
DJCJ14 <dekfrek@gmail.com>
2022-12-05 08:33:18 -0600

    add meal view controller basic function

--- FinalProject_MealTracker/AddViewController.swift
+++ FinalProject_MealTracker/AddViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class AddViewController: UIViewController {
     
     
@@ -35,6 +36,7 @@ class AddViewController: UIViewController {
     */
     
     @IBAction func addMealAction(_ sender: Any) {
+        AppData.meals.append(Meal(name: nameTextFieldOutlet.text!, price: Double(priceTextFieldOutlet.text!)!, cal: Int(calTextFieldOutlet.text!)!, day: Meal.Day.sunday))
     }
     
 
