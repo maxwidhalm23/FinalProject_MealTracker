4ef884a99c44c96516249d0085ce08dde416be12
Max Widhalm <max.widhalm23@d155.org>
2022-12-05 08:37:23 -0600

    added segue actions

--- FinalProject_MealTracker/ViewController.swift
+++ FinalProject_MealTracker/ViewController.swift
@@ -12,6 +12,8 @@ class AppData {
 }
 
 class ViewController: UIViewController {
+    
+    
 
     var test = 0
     var test2 = 7
@@ -21,6 +23,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addMealAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "addMealSegue", sender: nil)
+    }
+    
+    @IBAction func mealListAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "listSegue", sender: nil)
+    }
+    
 }
 
