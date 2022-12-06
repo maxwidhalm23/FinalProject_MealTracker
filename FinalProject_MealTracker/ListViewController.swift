065f1eecde3e82f1acd1eb3a0147bf0e12d704e1
DJCJ14 <dekfrek@gmail.com>
2022-12-06 07:57:22 -0600

    meals are deletable now

--- FinalProject_MealTracker/ListViewController.swift
+++ FinalProject_MealTracker/ListViewController.swift
@@ -32,7 +32,16 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            AppData.meals.remove(at: indexPath.row)
+            tableViewOutlet.reloadData()
+        }
+    }
     
 
 }
